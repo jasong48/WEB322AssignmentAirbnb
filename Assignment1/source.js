c7083906fdfca1b17b1ed8a89d8fcd42e7594351
Jason Goldenberg <jasongoldenberg497@gmail.com>
2021-07-01 15:41:27 -0400

    First commit

---
+++ Assignment1/source.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+
+
+app.engine('hbs', exphbs({defaultLayout: '/home', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+//routing
+app.get("/", (req, res) => {
+    res.render("home", { layout: false }); 
+  });
+
+  app.get("/home", (req, res) => {
+    res.render("home", { layout: false }); 
+  });
+
+//routing
+app.get("/room", (req, res) => {
+    res.render("room", { layout: false }); 
+  });
+
+//routing
+app.get("/reg", (req, res) => {
+    res.render("reg", { layout: false }); 
+  });
+
+app.listen(8080, () => {
+    console.log('Server is starting at port ', 8080);
+})
+
